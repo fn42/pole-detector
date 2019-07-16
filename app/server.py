2db186caa32249e4c04a9d55aa07ec6d3e3e2e04
anatoly <tolitius@gmail.com>
2019-07-16 15:49:32 -0400

    links to pole detector

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'http://gitpod.com/drop/pole-detector.pkl'
+export_file_name = 'pole-detector.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['poles', 'no poles']
 path = Path(__file__).parent
 
 app = Starlette()

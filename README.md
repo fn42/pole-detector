6cec223bd94ac31979b0fb4a758c74322e895bfe
anatoly <tolitius@gmail.com>
2019-07-17 00:21:55 -0400

    ui face lift

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Starter for deploying [fast.ai](https://www.fast.ai) models on [Render](https://render.com)
 
+> (semi) fork of: https://github.com/render-examples/fastai-v3
+
 This repo can be used as a starting point to deploy [fast.ai](https://github.com/fastai/fastai) models on Render.
 
 The sample app described here is up at https://fastai-v3.onrender.com. Test it out with bear images!
--- app/static/client.js
+++ app/static/client.js
@@ -1,24 +1,28 @@
 var el = x => document.getElementById(x);
+$('#analyze-button').hide();
+$('.result').hide();
 
 function showPicker() {
+  $('.result').hide();
   el("file-input").click();
 }
 
 function showPicked(input) {
-  el("upload-label").innerHTML = input.files[0].name;
   var reader = new FileReader();
   reader.onload = function(e) {
     el("image-picked").src = e.target.result;
     el("image-picked").className = "";
   };
   reader.readAsDataURL(input.files[0]);
+
+  $('#analyze-button').show();
 }
 
 function analyze() {
   var uploadFiles = el("file-input").files;
   if (uploadFiles.length !== 1) alert("please select a file to analyze!");
 
-  el("analyze-button").innerHTML = "analyzing...";
+  el("analyze-button").innerHTML = "uploading and checking...";
   var xhr = new XMLHttpRequest();
   var loc = window.location;
   xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
@@ -30,9 +34,11 @@ function analyze() {
     if (this.readyState === 4) {
       console.log("got response from the server:" + e.target.responseText)
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `result = ${response["result"]}`;
+      $('.result').text('I see ' + response.result.replace('-', ' '));
+      $('.result').show();
+      $('#analyze-button').hide();
+      el("analyze-button").innerHTML = "check for poles";
     }
-    el("analyze-button").innerHTML = "analyze";
   };
 
   var fileData = new FormData();
